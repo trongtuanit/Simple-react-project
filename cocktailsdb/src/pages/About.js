ccffa3e46ddf524b841abff54ed9391f8d02c2d7
Cloud_Forest <61158187+trongtuanit@users.noreply.github.com>
2021-07-25 00:22:47 +0700

    cocktaildb

---
+++ cocktailsdb/src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="about">
+      <h1 className="about-title">About Us</h1>
+      <p className="about-description">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
+        nesciunt ullam sapiente saepe magnam et nemo maiores dolorem tempora
+        quasi nam inventore reiciendis provident, id minima? Modi, minima
+        inventore! Facere!
+      </p>
+    </div>
+  );
+};
+export default About;
